5989c265ea80a8209471cfd2fc84f36909361261
maninageswarv <maninageswarv@gmail.com>
2025-06-27 14:17:26 +0000

    feat: Added database values in db.js

--- src/lib/database/db.js
+++ src/lib/database/db.js
@@ -1,13 +1,25 @@
 import mysql from 'mysql2/promise';
+/*
+ the below import is coming .env file but it is not pushed to git so used the
+ actual values in below query function to connect to database
+ */
+
+// @ts-ignore
 import { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } from '$env/static/private';
 
 // @ts-ignore
 export async function query({ query, values = [] }) {
     const dbconnection = await mysql.createConnection({
-        host: DATABASE_HOST,
-        user: DATABASE_USER,
-        password: DATABASE_PASSWORD,
-        database: DATABASE_NAME
+        /* 
+        the below values are the values of the mysql database which is installed in
+        windows laptop you have laptop replacement right then install mysql in mac
+        and create a database named 'company' and a table named 'employee' 
+        with columns "EmpID, EmpName, EmpAge, EmpGender"
+        */
+        host: 'localhost',
+        user: 'root',
+        password: 'root@2023#',
+        database: 'company'
     });
 
     try {
